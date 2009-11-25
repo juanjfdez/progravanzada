e2851df352092e2fcd6f6009ba9d8fa31adfca59
juanjfdez <j.j.fdez@hotmail.com>
2009-11-25 14:26:53 -0800

    mod 1.7 reporte

--- WindowsFormsApplication5/WindowsFormsApplication5/frmReportes.Designer.cs
+++ WindowsFormsApplication5/WindowsFormsApplication5/frmReportes.Designer.cs
@@ -51,6 +51,7 @@
             this.Controls.Add(this.crvReportes);
             this.Name = "frmReportes";
             this.Text = "Reporte";
+            this.Load += new System.EventHandler(this.frmReportes_Load);
             this.ResumeLayout(false);
 
         }
--- WindowsFormsApplication5/WindowsFormsApplication5/frmReportes.cs
+++ WindowsFormsApplication5/WindowsFormsApplication5/frmReportes.cs
@@ -6,14 +6,47 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using CrystalDecisions.Shared;
+using CrystalDecisions.CrystalReports.Engine;
 
 namespace WindowsFormsApplication5
 {
     public partial class frmReportes : Form
     {
+        protected TableLogOnInfo loginfo = new TableLogOnInfo();
+        crCliente reportecliente=new crCliente();
+        ReportDocument Reporte = new ReportDocument();
         public frmReportes()
         {
             InitializeComponent();
         }
+
+        private void setlogoninfo()
+        {
+            
+             try
+             {
+                 loginfo.ConnectionInfo.IntegratedSecurity = true;
+                 loginfo.ConnectionInfo.ServerName=".\\SQLEXPRESS";
+                 loginfo.ConnectionInfo.DatabaseName="PROYECTO";
+                 //loginfo.ConnectionInfo.UserID = "sa";
+                 //loginfo.ConnectionInfo.Password = "1234";
+             }
+             catch(Exception ex)
+             {
+                 MessageBox.Show(ex.Message);
+             }
+
+        }
+
+        private void frmReportes_Load(object sender, EventArgs e)
+        {
+            setlogoninfo();
+            reportecliente.Database.Tables[0].ApplyLogOnInfo(loginfo);
+
+            //que el viewer se iguale al source 
+            crvReportes.ReportSource=reportecliente;
+        }
+
     }
 }
